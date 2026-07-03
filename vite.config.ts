4f0dce3af56896bf462a0b1cd0317eb58e37b8a1
IntegrationGOAT <workpurpose086@gmail.com>
2026-07-03 23:24:17 +0530

    everything working

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {}
+  server: {
+    proxy: {
+      '/api/huggingface': {
+        target: 'https://router.huggingface.co',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/huggingface/, '/v1/chat/completions'),
+      },
+    },
+  },
 })
